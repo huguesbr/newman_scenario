d376359d7b9fb24d42371bfdbfa83f90ee47c8c8
Hugues Bernet-Rollande <hugues@xdev.fr>
2020-04-19 18:49:50 +0200

    adding support for workspace

--- lib/newman_scenario/scenario.rb
+++ lib/newman_scenario/scenario.rb
@@ -10,9 +10,10 @@ module NewmanScenario
   class Scenario
     DEFAULT_CUSTOM_SCENARIOS_FILE_PATH = 'newman_scenarios.json'.freeze
     DEFAULT_LAST_SCENARIO_FILE_PATH = '/tmp/last_newman_scenario.json'.freeze
+    DEBUG = true
 
     @default_collection_id = ENV['NEWMAN_SCENARIO_COLLECTION_ID']
-    @default_environment_ids = nil
+    @default_environments = nil
     @default_api_key = ENV['POSTMAN_API_KEY']
     @default_custom_scenarios_file_path = ENV['NEWMAN_SCENARIO_CUSTOM_COLLECTION_FILE_PATH'] || DEFAULT_CUSTOM_SCENARIOS_FILE_PATH
     @default_last_scenario_file_path = ENV['NEWMAN_SCENARIO_LAST_SCENARIO_FILE_PATH'] || DEFAULT_LAST_SCENARIO_FILE_PATH
@@ -20,21 +21,31 @@ module NewmanScenario
     class << self
       attr_accessor :default_api_key
       attr_accessor :default_collection_id
-      attr_accessor :default_environment_ids
+      attr_accessor :default_environments
       attr_accessor :default_custom_scenarios_file_path
       attr_accessor :default_last_scenario_file_path
 
-      def configure(default_api_key: nil, default_collection_id: nil, default_environment_ids: nil, default_custom_scenarios_file_path: nil, default_last_scenario_file_path: nil)
+      def configure(default_api_key: nil, default_collection_id: nil, default_environments: nil, default_custom_scenarios_file_path: nil, default_last_scenario_file_path: nil)
         self.default_api_key = default_api_key || prompt.ask('Postman API Key (https://YOURPOSTMAN.postman.co/settings/me/api-keys):', value: ENV['POSTMAN_API_KEY'].to_s)
-        collections = fetch_postman('/collections', api_key: self.default_api_key).parsed_response&.fetch('collections', nil) || []
+        collections = nil
+        environments = nil
+        if prompt.yes?('Using workspace?')
+          workspaces = fetch_postman('/workspaces', api_key: self.default_api_key).parsed_response&.fetch('workspaces', nil) || []
+          workspaces = workspaces.map { |workspace| workspace.slice('name', 'id').values }.to_h
+          workspace = prompt.select('Workspace', workspaces)
+          workspace = fetch_postman("/workspaces/#{workspace}", api_key: self.default_api_key).parsed_response&.fetch('workspace', nil) || {}
+          collections = workspace['collections']
+          environments = workspace['environments']
+        end
+        collections ||= fetch_postman('/collections', api_key: self.default_api_key).parsed_response&.fetch('collections', nil) || []
         collections = collections.map { |collection| collection.slice('name', 'id').values }.to_h
         self.default_collection_id = default_collection_id || prompt.select('Postman Collection', collections, default: 1)
-        self.default_environment_ids = default_environment_ids
-        unless self.default_environment_ids
-          environments = fetch_postman('/environments', api_key: self.default_api_key).parsed_response&.fetch('environments', nil) || []
+        self.default_environments = default_environments
+        unless self.default_environments
+          environments ||= fetch_postman('/environments', api_key: self.default_api_key).parsed_response&.fetch('environments', nil) || []
           environments = environments.map { |environment| environment.slice('name', 'id').values }.to_h
           environment_ids = prompt.multi_select('Postman Collection', environments)
-          self.default_environment_ids = environments.select { |_, id| environment_ids.include?(id) }
+          self.default_environments = environments.select { |_, id| environment_ids.include?(id) }
         end
         self.default_custom_scenarios_file_path = default_custom_scenarios_file_path || prompt.ask('Custom scenarios file path:', value: DEFAULT_CUSTOM_SCENARIOS_FILE_PATH)
         self.default_last_scenario_file_path = default_last_scenario_file_path || prompt.ask('Last scenario file path:', value: DEFAULT_LAST_SCENARIO_FILE_PATH)
@@ -42,7 +53,7 @@ module NewmanScenario
           envs = {
             POSTMAN_API_KEY: self.default_api_key,
             NEWMAN_SCENARIO_COLLECTION_ID: self.default_collection_id,
-            NEWMAN_SCENARIO_ENVIRONMENTS: self.default_environment_ids.to_json,
+            NEWMAN_SCENARIO_ENVIRONMENTS: self.default_environments.to_json,
             NEWMAN_SCENARIO_CUSTOM_COLLECTION_FILE_PATH: self.default_custom_scenarios_file_path,
             NEWMAN_SCENARIO_LAST_SCENARIO_FILE_PATH: self.default_last_scenario_file_path,
           }
@@ -51,7 +62,7 @@ module NewmanScenario
             existing_lines.each { |line| file.puts line }
             file.puts "POSTMAN_API_KEY: #{self.default_api_key}"
             file.puts "NEWMAN_SCENARIO_COLLECTION_ID: #{self.default_collection_id}"
-            file.puts "NEWMAN_SCENARIO_ENVIRONMENTS: #{self.default_environment_ids.to_json}"
+            file.puts "NEWMAN_SCENARIO_ENVIRONMENTS: #{self.default_environments.to_json}"
             file.puts "NEWMAN_SCENARIO_CUSTOM_COLLECTION_FILE_PATH: #{self.default_custom_scenarios_file_path}"
             file.puts "NEWMAN_SCENARIO_LAST_SCENARIO_FILE_PATH: #{self.default_last_scenario_file_path}"
           end
@@ -59,6 +70,7 @@ module NewmanScenario
       end
 
       def fetch_postman(url_path, expected_response_codes: [200], api_key: nil)
+        puts "fetching #{url_path}" if DEBUG
         response = HTTParty.get("https://api.getpostman.com#{url_path}", headers: { 'X-Api-Key' => api_key})
         raise Error, "Invalid response code: #{response.code}" unless expected_response_codes.include?(response.code)
 
@@ -73,18 +85,18 @@ module NewmanScenario
     end
 
     attr_accessor :collection_id
-    attr_accessor :environment_ids
+    attr_accessor :environments
     attr_accessor :api_key
     attr_accessor :custom_collection_file_path
     attr_accessor :last_scenario_file_path
 
-    def initialize(collection_id: nil, environment_ids: nil, api_key: nil, custom_collection_file_path: nil, last_scenario_file_path: nil)
+    def initialize(collection_id: nil, environments: nil, api_key: nil, custom_collection_file_path: nil, last_scenario_file_path: nil)
       self.collection_id ||= self.class.default_collection_id
       raise ConfigurationError, 'Missing Collection Id' unless self.collection_id
 
-      self.environment_ids ||= self.class.default_environment_ids
-      self.environment_ids ||= JSON.parse(ENV['NEWMAN_SCENARIO_ENVIRONMENTS'], symbolize_names: true) if ENV['NEWMAN_SCENARIO_ENVIRONMENTS']
-      raise ConfigurationError, 'Missing Environment Ids' unless self.environment_ids
+      self.environments ||= self.class.default_environments
+      self.environments ||= JSON.parse(ENV['NEWMAN_SCENARIO_ENVIRONMENTS'], symbolize_names: true) if ENV['NEWMAN_SCENARIO_ENVIRONMENTS']
+      raise ConfigurationError, 'Missing Environment Ids' unless self.environments
 
       self.api_key ||= self.class.default_api_key
       raise ConfigurationError, 'Missing Postman API Key' unless self.api_key
@@ -105,8 +117,8 @@ module NewmanScenario
     def run(environment_name: nil, scenario_name: nil, bail: true, no_prompt: false)
       return if `which newman`.empty? && !prompt_to_install_newman
 
-      environment = environment_ids[environment_name.to_sym] if environment_name
-      environment ||= prompt.select("Environment", environment_ids, default: 1)
+      environment = environments[environment_name.to_sym] if environment_name
+      environment ||= prompt.select('Environment', environments, default: 1)
       load_postman_environment(environment, no_prompt: no_prompt)
       collection = JSON.parse(File.read("/tmp/postman-collection-#{collection_id}.json"), symbolize_names: true)[:collection]
       unless File.exist?(last_scenario_file_path) && (!scenario_name && prompt.yes?('Replay last scenario?'))
@@ -123,7 +135,7 @@ module NewmanScenario
           all_request_names = extract_all_requests.call(collection, '')
           loop do
             scenario_requests.delete('duplicate')
-            scenario_requests += prompt.multi_select("Requests (type to filter prefix, choose duplicate to perform action multiple times)", ['duplicate'] + all_request_names, cycle: true, filter: true)
+            scenario_requests += prompt.multi_select('Requests (type to filter prefix, choose duplicate to perform action multiple times)', ['duplicate'] + all_request_names, cycle: true, filter: true)
             break unless scenario_requests.include?('duplicate')
 
           end
@@ -194,7 +206,7 @@ module NewmanScenario
         if no_prompt
           false
         else
-          !prompt.no?("Refetch postman config?")
+          !prompt.no?('Refetch postman config?')
         end
       if File.file?("/tmp/postman-environment-#{environment}.json") && !reload
         prompt.ok "reusing env /tmp/postman-environment-#{environment}.json"
@@ -203,7 +215,7 @@ module NewmanScenario
         fetch_postman_to_file("/environments/#{environment}", "/tmp/postman-environment-#{environment}.json")
       end
       if File.file?('/tmp/postman-collection.json') && !reload
-        prompt.ok "reusing collection /tmp/postman-collection.json"
+        prompt.ok 'reusing collection /tmp/postman-collection.json'
       else
         prompt.ok "fetching collection #{collection_id}"
         fetch_postman_to_file("/collections/#{collection_id}", "/tmp/postman-collection-#{collection_id}.json")
