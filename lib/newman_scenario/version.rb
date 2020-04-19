bfae1477a1eba56bfa870a6a6697152c6bd69dae
Hugues Bernet-Rollande <hugues@xdev.fr>
2020-04-19 15:42:29 +0200

    `NewmanScenario` - `NewmanScenario::Scenario` main class to 	- configure `NewmanScenario` using `NewmanScenario::Scenario.default_xxx = 'xxx'` 	- run a scenario using `NewmanScenario::Scenario.run(environment_name: 'staging', scenario_name: 'Signup')` - `NewmanScenario::CLI` link to the `newman_scenario` command line 	- `newman_scenario staging Signup`

---
+++ lib/newman_scenario/version.rb
@@ -0,0 +1,3 @@
+module NewmanScenario
+  VERSION = "0.1.0"
+end
---
+++ spec/newman_scenario_spec.rb
@@ -0,0 +1,5 @@
+RSpec.describe NewmanScenario do
+  it "has a version number" do
+    expect(NewmanScenario::VERSION).not_to be nil
+  end
+end
